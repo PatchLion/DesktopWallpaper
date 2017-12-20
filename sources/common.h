9808e9430daecb0522044e2d09deeaa8b50415cd
PatchLion <zxfd1434@sina.com>
2017-12-20 17:23:48 +0800

    .

--- sources/common.h
+++ sources/common.h
@@ -3,7 +3,7 @@
 
 #include <QString>
 
-static const QString kDefaultHost = "http://127.0.0.1";
+static const QString kDefaultHost = "http://www.patchlion.cn";
 //static const QString kAPIHost = "/host"; //查询主机API
 static const QString kAPIClassifies = "/classifies"; //图片分类查询API
 static const QString kAPIItemsByClassifyIDAndPageIndex = "/classifies/%1/%2"; //图片分类分页查询API
